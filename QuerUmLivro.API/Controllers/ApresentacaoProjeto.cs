2b17daa3a3f9048f356f308722d21df39985fa06
Jorge <jorgelo.dev@gmail.com>
2024-01-29 20:16:16 -0300

    - exemplo apresentacao

---
+++ QuerUmLivro.API/Controllers/ApresentacaoProjeto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuerUmLivro.API.Controllers
+{
+    [ApiController]
+    [Route("Apresentacao")]
+    public class ApresentacaoProjeto : Controller
+    {
+        [HttpGet()]
+        public IActionResult Ola()
+        {
+            return Ok("Tech Challenge Fase 2");
+        }
+    }
+}
